3a5bfeb013273457f1a9ab33c69fe3cecfb9f6a9
surrealist <suthep@gfbd.co.th>
2021-02-27 10:54:44 +0700

    Refactor test setup
    Create AppBuilder to configre  test context.

---
+++ GreatFriends.SmartHotel.Tests/AppBuilder.cs
@@ -0,0 +1,43 @@
+﻿using GreatFriends.SmartHoltel.Models;
+using GreatFriends.SmartHoltel.Services;
+using GreatFriends.SmartHoltel.Services.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GreatFriends.SmartHotel.Tests
+{
+  internal class AppBuilder
+  {
+    private App app;
+
+    public AppBuilder()
+    {
+      var options = new DbContextOptionsBuilder<AppDb>()
+                     .UseInMemoryDatabase($"db-{Guid.NewGuid()}")
+                     .Options;
+      var db = new AppDb(options);
+      app = new App(db);
+    }
+
+    public AppBuilder WithSingleRoom()
+    {
+      var roomTypeS1 = new RoomType { Code = "S1", Name = "Single bed", Price = 1_000m };
+      var room501 = new Room
+      {
+        Id = 501,
+        FloorNo = 5,
+        AreaSquareMeters = 30,
+        RoomTypeCode = "S1"
+      }; //, RoomType = roomTypeS1 };
+      app.RoomTypes.Add(roomTypeS1);
+      app.Rooms.Add(room501);
+      app.SaveChanges();
+
+      return this;
+    }
+
+    public App Build() => app;
+  }
+}
--- GreatFriends.SmartHotel.Tests/ReservationServiceTest.cs
+++ GreatFriends.SmartHotel.Tests/ReservationServiceTest.cs
@@ -14,31 +14,23 @@ namespace GreatFriends.SmartHotel.Tests
   {
     public class Create
     {
-
+     
       [Fact]
       public void Simple()
       {
-        // arrange
-        var options = new DbContextOptionsBuilder<AppDb>()
-                      .UseInMemoryDatabase($"db-{Guid.NewGuid()}")
-                      .Options;
-        var db = new AppDb(options);
-        var app = new App(db);
+        var app = new AppBuilder()
+                  .WithSingleRoom()
+                  .Build();
 
+        var room501 = app.Rooms.Find(501);
         var dt1 = new DateTime(2021, 2, 20);
         var dt2 = new DateTime(2021, 3, 20);
-        var roomTypeS1 = new RoomType { Code = "S1", Name = "Single bed", Price = 1_000m };
-        var room501 = new Room { Id = 501, FloorNo = 5, AreaSquareMeters = 30, RoomTypeCode = "S1" }; //, RoomType = roomTypeS1 };
-        app.RoomTypes.Add(roomTypeS1);
-        app.Rooms.Add(room501);
-        app.SaveChanges();
-
         var model = new Reservation
         {
           CustomerName = "Alice",
           Mobile = "999",
           Email = "alice@c.com",
-          RoomId = 501,
+          RoomId = room501.Id,
           Room = room501,
           CheckInDate = dt1,
           CheckOutDate = dt2,
@@ -56,6 +48,8 @@ namespace GreatFriends.SmartHotel.Tests
         Assert.Equal(dt2, r.CheckOutDate);
         Assert.Equal(1, app.Reservations.All().Count());
       }
+
+
     }
   }
 }
